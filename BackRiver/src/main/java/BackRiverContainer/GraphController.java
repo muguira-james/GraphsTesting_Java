29b6d028dd3f3de75c0e2f9e5f9a3a3b17685b36
James Muguira <muguira.james@gmail.com>
2018-10-15 14:24:10 -0400

    reflect what is in the article

--- BackRiver/src/main/java/BackRiverContainer/GraphController.java
+++ BackRiver/src/main/java/BackRiverContainer/GraphController.java
@@ -1,21 +1,34 @@
 package BackRiverContainer;
 
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+
 import grandview.Edge;
 import grandview.Vertex;
 import grandview.GraphAdjList;
 
 @RestController
+
 public class GraphController {
 	 private static final String template = "%s!";
 	    private final AtomicInteger counter = new AtomicInteger();
 
+	    private GraphAdjList adjListMap = new GraphAdjList();
+	    
 	    @RequestMapping("/getVertexTest")
 	    public Vertex getVertexTest(@RequestParam(value="name", defaultValue="Jim") String name) {
 	    	
@@ -73,17 +86,35 @@ public class GraphController {
 	    	return adjList;
 	    }
 	    
-	    @RequestMapping("/getQwestTest")
+
+	    @CrossOrigin(origins = "http://localhost:3000")
+	    @GetMapping("/getQwestTest")
 	    public GraphAdjList getQuestTest() {
 	    	
 	    	GraphAdjList adjList = new GraphAdjList();
 			
 			Vertex vJ = new Vertex("Jasper", 0);
+			vJ.addToStuffCarried("sword");
+
 			Vertex vT = new Vertex("Tom", 1);
+			vT.addToStuffCarried("sack");
+			vT.setStrength(11);
+			
 			Vertex vE = new Vertex("Emma", 2);
+			vE.addToStuffCarried("wand");
+			vE.setStrength(11);
+			
 			Vertex vC = new Vertex("Charlie", 3);
+			vC.addToStuffCarried("wand");
+			vC.setStrength(7);
+			
 			Vertex vB = new Vertex("Ben", 4);
+			vB.addToStuffCarried("sandwich");
+			vB.setStrength(17);
+			
 			Vertex vO = new Vertex("Olivia", 5);
+			vO.addToStuffCarried("ring");
+			vO.setStrength(17);
 			
 			adjList.addVertex(vJ);
 			adjList.addVertex(vT);
@@ -113,4 +144,38 @@ public class GraphController {
 			
 			return adjList;
 	    }
+	    
+	    @CrossOrigin(origins = "http://localhost:3000")
+	    @GetMapping("/AddVertex")
+	    public String AddVertex(@RequestParam(value="vertexName", defaultValue="Jim") String name) {
+	    	
+	    	Vertex v = new Vertex();
+	    	v.setName(name);
+	    	adjListMap.addVertex(v);
+	    	
+	    	return v.getName();
+	    }
+	    
+	    @CrossOrigin(origins = "http://localhost:3000")
+	    @GetMapping("/AddEdge")
+	    public String AddEdge(@RequestParam String vertexName1, @RequestParam String vertexName2) {
+	    	
+	
+	        Vertex v = new Vertex();
+	    	v.setName(vertexName1);
+	    	
+	    	Vertex w = new Vertex();
+	    	w.setName(vertexName2);
+	    	
+	    	adjListMap.addEdge(v, w);
+	    	
+	    	return "Edge Added";
+	    }
+	    
+	    @CrossOrigin(origins = "http://localhost:3000")
+	    @GetMapping("/getGraph")
+	    public GraphAdjList getGraph() {
+	    	return adjListMap;
+	    }
+	    
 }
